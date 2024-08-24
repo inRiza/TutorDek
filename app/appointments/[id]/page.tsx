037933dc6b6c66225d4ca5dac42ad8898e2fd84f
farrelathalla <farrelxag@gmail.com>
2024-08-25 05:32:40 +0700

    fix: fix appointment bug

--- app/appointments/[id]/page.tsx
+++ app/appointments/[id]/page.tsx
@@ -1,6 +1,9 @@
-"use client"
+"use client";
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { IoMdArrowDropleft } from "react-icons/io";
+import Link from 'next/link';
+import { useToast } from '@/components/ui/use-toast';
 
 export enum Weekday {
   Monday = "Monday",
@@ -16,6 +19,14 @@ export enum Media {
   Online = "Online",
   Offline = "Offline",
   Hybrid = "Hybrid",
+}
+
+export enum Duration {
+  ONE_HOUR = "1",
+  ONE_AND_HALF = "1.5",
+  TWO_HOURS = "2",
+  TWO_AND_HALF = "2.5",
+  THREE_HOURS = "3",
 }
 
 type TimeRange = {
@@ -34,12 +45,14 @@ type AppointmentType = {
     fullName: string;
     jurusan: string;
     description: string;
+    calendar: string;
   };
   availableDays: { day: Weekday; timeRange: { start: string; end: string } }[];
   media: Media;
-};
+};  
 
 const AppointmentDetailPage = () => {
+  const { toast } = useToast();
   const pathname = usePathname();
   const id = pathname.split('/').pop(); // Extract the ID from the pathname
   const [appointment, setAppointment] = useState<AppointmentType | null>(null);
@@ -47,6 +60,8 @@ const AppointmentDetailPage = () => {
   const [selectedDate, setSelectedDate] = useState<string>('');
   const [selectedTime, setSelectedTime] = useState<string>('');
   const [peopleCount, setPeopleCount] = useState<number>(1);
+  const [selectedDuration, setSelectedDuration] = useState<Duration>(Duration.ONE_HOUR);
+  const [selectedMediaOption, setSelectedMediaOption] = useState<Media | null>(null);
 
   useEffect(() => {
     if (id) {
@@ -61,8 +76,8 @@ const AppointmentDetailPage = () => {
   }, [id]);
 
   const handleAddAppointment = async () => {
-    if (!selectedDate || !selectedTime) {
-      alert('Please select a date and time.');
+    if (!selectedDate || !selectedTime ) {
+      alert('Please select a date, time, and media option.');
       return;
     }
 
@@ -71,6 +86,8 @@ const AppointmentDetailPage = () => {
       selectedDate: new Date(selectedDate),
       selectedTime: new Date(`${selectedDate}T${selectedTime}:00`),
       peopleCount,
+      duration: selectedDuration,
+      media: selectedMediaOption || appointment?.media,
     };
 
     const response = await fetch('/api/appointment-assignments', {
@@ -82,87 +99,133 @@ const AppointmentDetailPage = () => {
     });
 
     if (response.ok) {
-      alert('Appointment added successfully!');
+      toast({
+        title:"Appointment added succesfully!",
+        description: "Check notification for further information",
+        variant:"default",
+      })
       setIsModalOpen(false);
     } else {
-      alert('Failed to add appointment.');
+      toast({
+        title:"Failed to add appointment!",
+        description: "Spmething went wrong",
+        variant:"destructive",
+      })
     }
   };
 
   if (!appointment) {
-    return <div>Loading...</div>;
+    return <div className='w-full min-h-screen h-full bg-[#1C1C1C] flex flex-col items-center justify-center text-white font-bold'>
+        Loading...
+      </div>;
   }
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">{appointment.title}</h1>
-      <p className="mb-4">{appointment.description}</p>
-      <h4 className="font-semibold mb-2">Creator Information:</h4>
-      {appointment.creator ? (
-        <>
-          <p>Name: {appointment.creator.fullName}</p>
-          <p>Jurusan: {appointment.creator.jurusan}</p>
-          <p>Description: {appointment.creator.description}</p>
-        </>
-      ) : (
-        <p>Creator information not available</p>
-      )}
-      <h4 className="font-semibold mt-4 mb-2">Available Days and Times:</h4>
-      <ul className="mb-4">
-        {appointment.availableDays?.length > 0 ? (
-          appointment.availableDays.map((dayTimeRange, index) => (
-            <li key={index} className="mb-2">
-              {dayTimeRange.day}: {dayTimeRange.timeRange.start} - {dayTimeRange.timeRange.end}
-            </li>
-          ))
+    <section>
+      <div className='w-full min-h-screen h-full bg-[#1C1C1C] flex flex-col p-10 md:p-20'>
+        <Link href="#" onClick={(e) => { e.preventDefault(); window.history.back(); }} className='flex gap-3 items-center'>
+          <IoMdArrowDropleft className='text-[#A1A1A1] text-xl'/>
+          <p className='text-[#A1A1A1] font-semibold'> Back </p>
+        </Link>
+        <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-[#7879ED] mt-5">{appointment.title}</h1>
+        <p className='text-white font-semibold mt-3 text-base md:text-lg'> Description </p>
+        <p className='text-[#A1A1A1] text-sm md:text-base'> {appointment.description} </p>
+        {appointment.creator ? (
+          <>
+            <h1 className='mt-10 text-white font-bold text-xl md:text-2xl lg:text-4xl '> {appointment.creator.fullName}</h1>
+            <h2 className='text-[#A1A1A1] font-semibold text-lg md:text-xl lg:text-2xl'>{appointment.creator.jurusan}</h2>
+            <p className='text-[#7879ED] font-bold text-base md:text-lg xl:text-xl mt-3'> About the tutor </p>
+            <p className='text-white text-xs md:text-base'>{appointment.creator.description}</p>
+          </>
         ) : (
-          <p>No available days</p>
+          <p>Creator information not available</p>
         )}
-      </ul>
-      <p className="mb-4">Media: {appointment.media}</p>
-
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        Add Appointment
-      </button>
-
-      {/* Modal for adding an appointment */}
-      {isModalOpen && (
+        <div className='bg-[#E6BB30] p-6 rounded-xl mt-5 font-bold text-lg md:text-xl lg:text-2xl'>
+          <ul>
+            {appointment.availableDays?.length > 0 ? (
+              appointment.availableDays.map((dayTimeRange, index) => (
+                <li key={index} className="mb-2">
+                  {dayTimeRange.day} {dayTimeRange.timeRange.start} - {dayTimeRange.timeRange.end}
+                </li>
+              ))
+            ) : (
+              <p>No available days</p>
+            )}
+          </ul>
+          <p className='font-semibold text-base md:text-lg lg:text-xl'>{appointment.media}</p>
+          <div className='flex justify-end gap-3'>
+            <button className="bg-black text-white px-5 py-3 rounded-full font-semibold mt-5 text-base md:text-lg lg:text-xl hover:bg-slate-800">
+              <Link href={appointment.creator.calendar ? appointment.creator.calendar : '' } target='_blank'>
+                Availability  
+              </Link>
+            </button>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="bg-[#7879ED] text-white px-5 py-3 rounded-full hover:bg-[#5658aa] font-semibold mt-5 text-base md:text-lg lg:text-xl "
+            >
+              Add Appointment
+            </button>
+            {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+          <div className="bg-[#292929] p-6 rounded-xl border-[1px] border-[#7879ED] shadow-lg w-80 relative">
             <span
-              className="absolute top-2 right-2 text-gray-500 cursor-pointer"
+              className="absolute top-2 right-4 text-gray-500 cursor-pointer text-4xl"
               onClick={() => setIsModalOpen(false)}
             >
               &times;
             </span>
-            <h2 className="text-xl font-semibold mb-4">Add Appointment</h2>
+            <h2 className="text-2xl font-bold mb-4 text-[#7879ED]">Add Appointment</h2>
             <div className="mb-4">
-              <label className="block mb-2">Select Date:</label>
+              <label className="block mb-2 font-semibold text-base text-white">Select Date:</label>
               <input
                 type="date"
                 value={selectedDate}
                 onChange={(e) => setSelectedDate(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border-[1px] p-2 rounded-lg font-semibold text-base text-white bg-[#292929] border-[#A1A1A1] focus:outline-[#7879ED] w-full"
               />
             </div>
             <div className="mb-4">
-              <label className="block mb-2">Select Time:</label>
+              <label className="block mb-2 font-semibold text-base text-white">Select Time:</label>
               <input
                 type="time"
                 value={selectedTime}
                 onChange={(e) => setSelectedTime(e.target.value)}
-                className="border p-2 rounded w-full"
+                className="border-[1px] p-2 rounded-lg font-semibold text-base text-white bg-[#292929] border-[#A1A1A1] focus:outline-[#7879ED] w-full"
               />
             </div>
             <div className="mb-4">
-              <label className="block mb-2">People Count:</label>
+              <label className="block mb-2 font-semibold text-base text-white">Duration:</label>
+              <select
+                value={selectedDuration}
+                onChange={(e) => setSelectedDuration(e.target.value as Duration)}
+                className="border-[1px] p-2 rounded-lg font-semibold text-base text-white bg-[#292929] border-[#A1A1A1] focus:outline-[#7879ED] w-full"
+              >
+                {Object.values(Duration).map((duration) => (
+                  <option key={duration} value={duration}>
+                    {duration} hours
+                  </option>
+                ))}
+              </select>
+            </div>
+            {appointment.media === Media.Hybrid && (
+              <div className="mb-4">
+                <label className="block mb-2 font-semibold text-base text-white">Media Option:</label>
+                <select
+                  value={selectedMediaOption || appointment.media}
+                  onChange={(e) => setSelectedMediaOption(e.target.value as Media)}
+                  className="border-[1px] p-2 rounded-lg font-semibold text-base text-white bg-[#292929] border-[#A1A1A1] focus:outline-[#7879ED] w-full"
+                >
+                  <option value={Media.Online}>Online</option>
+                  <option value={Media.Offline}>Offline</option>
+                </select>
+              </div>
+            )}
+            <div className="mb-4">
+              <label className="block mb-2 font-semibold text-base text-white ">People Count:</label>
               <select
                 value={peopleCount}
                 onChange={(e) => setPeopleCount(parseInt(e.target.value))}
-                className="border p-2 rounded w-full"
+                className="border-[1px] p-2 rounded-lg font-semibold text-base text-white bg-[#292929] border-[#A1A1A1] focus:outline-[#7879ED] w-full"
               >
                 {[1, 2, 3, 4, 5].map((count) => (
                   <option key={count} value={count}>
@@ -173,14 +236,17 @@ const AppointmentDetailPage = () => {
             </div>
             <button
               onClick={handleAddAppointment}
-              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full"
+              className="bg-[#7879ED] text-white px-5 py-3 rounded-full hover:bg-[#5657ac] w-full text-lg"
             >
               Submit
             </button>
           </div>
         </div>
       )}
-    </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- app/appointments/page.tsx
+++ app/appointments/page.tsx
@@ -1,5 +1,8 @@
-"use client "
+"use client"
 import { useEffect, useState } from 'react';
+import Link from "next/link";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export enum AssignmentStatus {
   Pending = 'Pending',
@@ -7,63 +10,112 @@ export enum AssignmentStatus {
   Declined = 'Declined',
 }
 
+enum Duration {
+  ONE_HOUR = "1",
+  ONE_AND_HALF = "1.5",
+  TWO_HOURS = "2",
+  TWO_AND_HALF = "2.5",
+  THREE_HOURS = "3",
+} 
+
+const formatDuration = (duration: string): string => {
+  switch (duration) {
+    case "ONE_HOUR":
+      return "1 Hour";
+    case "ONE_AND_HALF":
+      return "1.5 Hours";
+    case "TWO_HOURS":
+      return "2 Hours";
+    case "TWO_AND_HALF":
+      return "2.5 Hours";
+    case "THREE_HOURS":
+      return "3 Hours";
+    default:
+      return "N/A"; // Fallback if needed
+  }
+};
+
 type AppointmentType = {
   id: number;
   appointment: {
+    id: number;
     title: string;
-    media: string;
     creator: {
       fullName: string;
       phoneNumber: string;
+      jurusan: string;
     };
   };
   selectedDate: string;
   selectedTime: string;
   peopleCount: number;
   status: AssignmentStatus;
+  duration: Duration;
+  media: string;
 };
 
 const UserDashboard = () => {
   const [appointments, setAppointments] = useState<AppointmentType[]>([]);
+  const days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
 
   useEffect(() => {
     const fetchAppointments = async () => {
       const response = await fetch('/api/user/appointments');
       const data = await response.json();
+      console.log("Appointments data:", data);
       setAppointments(data);
-    };
+    }; 
 
     fetchAppointments();
   }, []);
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">User Dashboard: Appointments</h1>
-      <ul>
-        {appointments.map((appointment) => (
-          <li key={appointment.id} className="mb-4 border p-4 rounded-lg">
-            <h2 className="text-lg font-semibold">{appointment.appointment.title}</h2>
-            <p>Tutor: {appointment.appointment.creator.fullName}</p>
-            <p>Date: {new Date(appointment.selectedDate).toLocaleDateString()}</p>
-            <p>Time: {new Date(appointment.selectedTime).toLocaleTimeString()}</p>
-            <p>Media: {appointment.appointment.media}</p>
-            <p>People Count: {appointment.peopleCount}</p>
-            <p>Status: {appointment.status}</p>
-
-            {appointment.status === AssignmentStatus.Accepted && (
-              <a
-                href={`https://wa.me/${appointment.appointment.creator.phoneNumber}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="mt-4 inline-block bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-              >
-                Contact Tutor
-              </a>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <Navbar/>
+      <div className='bg-[#1C1C1C] text-white min-h-screen h-full w-full flex flex-col'>
+        <div className='p-20'>
+          <h1 className="text-5xl font-bold my-10">Your <span className=' text-[#7879ED]'>Schedule</span></h1>
+          {appointments.length > 0 ? (
+            <ul>
+            {appointments.slice().reverse().map((appointment) => (
+              
+              
+              <li key={appointment.id} className="mb-5 border p-6 rounded-3xl border-[#7879ED] shadow-lg bg-[#292929] transition-all grid lg:grid-cols-[70%_30%] duration-500 z-10">
+                <Link href={`appointments/${appointment.appointment.id}`}>
+                  <h2 className="text-2xl font-bold text-[#7879ED] mb-2">{appointment.appointment.title}</h2>
+                  <p className='text-lg font-semibold leading-none'>{appointment.appointment.creator.fullName}</p>
+                  <p className='text-lg text-[#A1A1A1]'>{appointment.appointment.creator.jurusan}</p>
+                  <div className='flex flex-wrap text-[#A1A1A1]'>
+                    <p className='mr-8'>Day <span className='text-[#7879ED] text-lg font-bold mx-2 '>{days[new Date(appointment.selectedDate).getDay()]}</span></p>
+                    <p className='mr-8'>Time <span className='text-[#7879ED] text-lg font-bold mx-2'>{new Date(appointment.selectedTime).toLocaleTimeString(["ban", "id"]).slice(0,-3)} WIB</span></p>
+                    <p className='mr-8'>Duration <span className='text-[#7879ED] text-lg font-bold mx-2'>{formatDuration(appointment.duration)}</span></p>
+                    <p className='mr-8'>Place <span className='text-[#7879ED] text-lg font-bold mx-2'>{appointment.media}</span></p>
+                    <p className='mr-8'>Members <span className='text-[#7879ED] text-lg font-bold mx-2'>{appointment.peopleCount}</span></p>
+                  </div>
+                </Link>
+                <div className='font-semibold flex flex-col lg:items-center items-start justify-start lg:justify-center'>
+                  <p className={`text-xl md:text-center ${appointment.status == AssignmentStatus.Accepted ? 'text-[#519359]' : appointment.status == AssignmentStatus.Declined ? 'text-[#FF4646]' : 'text-[#A1A1A1]' }`}><span className='text-[#A1A1A1] text-base font-normal mr-2 md:hidden'>Status</span>{appointment.status}</p>
+                  {appointment.status === AssignmentStatus.Accepted && (
+                  <a
+                    href={`https://wa.me/${appointment.appointment.creator.phoneNumber}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block bg-[#7879ED] text-[#292929] px-4 py-2 rounded-full hover:bg-[#4444bb] my-4 md:mt-4 font-bold"
+                  >
+                    Send a message
+                  </a>
+                )}
+                </div>
+              </li>
+            ))}
+          </ul>
+          ): (
+            <div className='text-[#A1A1A1]'> There is no appointments </div>
+          )}
+        </div>
+      </div>
+      <Footer/>
+    </section>
   );
 };
 
